0fbc2e4027de6eeebab3e487bdefebf4ce652acf
Jesse Viitanen <essej.viitanen@gmail.com>
2020-12-09 20:58:56 +0100

    task1

--- Task1/task1.cpp
+++ Task1/task1.cpp
@@ -1,4 +1,101 @@
 #include <iostream>
+#include <vector>
+#include <string>
 #include "task1.h"
 using namespace std;
 
+void Engine::print()
+{
+	cout << "Engine:" << endl;
+	cout << "----------------------------------------" << endl;;
+	cout << "Volume(litres): " << sizeInLitres << endl;
+	cout << "Cylinders: " << numberOfCylinders << endl;
+	cout << "----------------------------------------\n" << endl;
+}
+
+void Body::print()
+{
+	cout << "Body:" << endl;
+	cout << "----------------------------------------" << endl;;
+	cout << "Color: " << color << endl;
+	cout << "Width: " << width << endl;
+	cout << "Height: " << height << endl;
+	cout << "----------------------------------------\n" << endl;
+}
+
+void Owner::print()
+{
+	cout << "Owner:" << endl;
+	cout << "----------------------------------------" << endl;;
+	cout << "Name: " << name << endl;
+	cout << "Adress: " << adress << endl;
+	cout << "Driver's license: " << boolalpha << allowedToDrive << endl;
+	cout << "----------------------------------------\n" << endl;
+}
+
+void MotorVehicle::print()
+{
+	owner.print();
+	body.print();
+	engine.print();
+	cout << "Tires:" << endl;
+	cout << "----------------------------------------" << endl;;
+	cout << "Number of tires: " << numberOfTires << endl;
+	cout << "Tire diameters: ";
+	for (int i = 0; i < numberOfTires; i++)
+	{
+		cout << tireDiameters << " ";
+	}
+	cout << endl;
+	//cout << "Tire diameter: " << tireDiameters << endl;
+	cout << "----------------------------------------\n" << endl;
+}
+
+int main()
+{
+	string name;
+	string adress;
+	bool drive = true;
+	string color;
+	float width, height, volume;
+	int cylinders, tires;
+	float* diameter = new float;
+	
+
+	//int vehicles;
+	//cout << "How many vehicles?: ";
+	//cin >> vehicles;
+
+	//for (int i = 0; i < vehicles; i++)
+	//{
+		//cout << "Vehicle " << i + 1 << ": " << endl;
+		cout << "----------------------------------------" << endl;
+		cout << "Name of owner: "; getline(cin, name);
+
+		cout << "Adress of owner: "; getline(cin, adress);
+
+		cout << "Driver's license? (1 for yes, 0 for no): "; cin >> drive;
+
+		cout << "Color of car: "; cin >> color;
+
+		cout << "Width of car: "; cin >> width;
+
+		cout << "Height of car: "; cin >> height;
+
+		cout << "Volume of engine(in litres): "; cin >> volume;
+
+		cout << "Number of cylinders: "; cin >> cylinders;
+
+		cout << "Number of tires: "; cin >> tires;
+
+		cout << "Diameter of tires: "; cin >> *diameter;
+
+		system("cls");
+
+	//}
+	
+	MotorVehicle car1(name, adress, drive, color, width, height, volume, cylinders, tires, diameter);
+	car1.print();
+
+	return 0;
+}
--- Task1/task1.h
+++ Task1/task1.h
@@ -1,1 +1,100 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Engine
+{
+public:
+	float sizeInLitres;
+	int numberOfCylinders;
+	Engine()
+	{
+		sizeInLitres = 0;
+		numberOfCylinders = 0;
+	}
+	Engine(float volume, int cylinders)
+	{
+		sizeInLitres = volume;
+		numberOfCylinders = cylinders;
+	}
+	void print();
+};
+
+class Body
+{
+public:
+	string color;
+	float width, height;
+	Body()
+	{
+		color = "white";
+		width = 1.5;
+		height = 1.5;
+	}
+	Body(string bcolor, float bwidth, float bheight)
+	{
+		color = bcolor;
+		width = bwidth;
+		height = bheight;
+	}
+	void print();
+};
+
+class Owner
+{
+public:
+	string name;
+	string adress;
+	bool allowedToDrive;
+	Owner()
+	{
+		name = "Raul";
+		adress = "Stekargatan";
+		allowedToDrive = true;
+	}
+	Owner(string oname, string oadress, bool driversLicense)
+	{
+		name = oname;
+		adress = oadress;
+		allowedToDrive = driversLicense;
+	}
+	void print();
+};
+
+class MotorVehicle
+{
+public:
+	Engine engine;
+	Body body;
+	Owner owner;
+	float* tireDiameters;
+	int numberOfTires;
+	MotorVehicle(string name, string adress, bool allowDrive, string color, float width, float height, float volume, int cylinders, int tires, float* tireDm)
+	{
+		//Owner
+		owner.name = name;
+		owner.adress = adress;
+		owner.allowedToDrive = allowDrive;
+
+		//Body
+		body.color = color;
+		body.width = width;
+		body.height = height;
+
+		//Engine
+		engine.sizeInLitres = volume;
+		engine.numberOfCylinders = cylinders;
+
+		//Tires
+		numberOfTires = tires;
+		for (int i = 0; i < numberOfTires; i++)
+		{
+			tireDiameters = tireDm;
+		}
+		//tireDiameters = tireDm;
+	}
+	void print();
+};
+
